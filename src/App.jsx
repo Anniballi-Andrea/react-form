a4740c9a34727db155420d3597e5f255b463ff54
Anniballi-Andrea <andreanniballi@gmail.com>
2025-11-10 12:45:38 +0100

    add bootsrap, an array of games and a component for the articles list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 
 function App() {
 
+  const games =
+    [
+      {
+        name: "monopoly",
+        id: 1
+      },
+      {
+        name: "exploding kittens",
+        id: 2
+      },
+      {
+        name: "risiko",
+        id: 2
+      }
+
+    ]
 
   return (
     <>
+      <div className="container">
 
+      </div>
     </>
   )
 }
